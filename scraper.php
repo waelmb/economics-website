a6463b5a180d5caf669b1868525417149d0ff57e
waelmb <wafm2007@gmail.com>
2020-03-27 14:49:54 -0500

    Create scraper.php

---
+++ scraper.php
@@ -0,0 +1,46 @@
+  <?php echo file_get_contents("html/header.html"); ?>
+  </head>
+  <body id="index">
+  <?php echo file_get_contents("html/header2.html"); ?>
+  <div class="wrapper">
+      <div class="container">
+        <!-- <div class="flickity">
+          <div><img src="images/index-flickity/building.png" alt="photo"></div>
+          <div><img src="images/index-flickity/clinic.png" alt="photo"></div>
+          <div><img src="images/index-flickity/social.png" alt="photo"></div>
+        </div> -->
+        <h1> Welcome to Interactive Economics! </h1>
+        <h2> About Us </h2>
+        <p>This website has been developed for the purpose of enriching economics curricula in high schools and universities. Students are encouraged to visualize the shifts associated with the economics concepts that they learn in their introduction and intermediate microeconomics and macroeconomics classes. The graphs that can be studied are displayed in the tab under the respective class. Please explore the different graphs and play around with the different factors to see how shifts in the graphs affect things like price, quantity, efficiency, and revenue. We hope you benefit from this resource, and happy studying!</p>
+        <h2> Our team </h2>
+        <h3> Omar Nabulsi </h3>
+        <p>This website has been developed for the purpose of enriching economics curricula in high schools and universities. Students are encouraged to visualize the shifts associated with the economics concepts that they learn in their introduction and intermediate microeconomics and macroeconomics classes. The graphs that can be studied are displayed in the tab under the respective class. Please explore the different graphs and play around with the different factors to see how shifts in the graphs affect things like price, quantity, efficiency, and revenue. We hope you benefit from this resource, and happy studying!</p>
+        <h3> Dana Malo </h3>
+        <p>This website has been developed for the purpose of enriching economics curricula in high schools and universities. Students are encouraged to visualize the shifts associated with the economics concepts that they learn in their introduction and intermediate microeconomics and macroeconomics classes. The graphs that can be studied are displayed in the tab under the respective class. Please explore the different graphs and play around with the different factors to see how shifts in the graphs affect things like price, quantity, efficiency, and revenue. We hope you benefit from this resource, and happy studying!</p>
+        <h3> Wael Mobeirek </h3>
+        <p>This website has been developed for the purpose of enriching economics curricula in high schools and universities. Students are encouraged to visualize the shifts associated with the economics concepts that they learn in their introduction and intermediate microeconomics and macroeconomics classes. The graphs that can be studied are displayed in the tab under the respective class. Please explore the different graphs and play around with the different factors to see how shifts in the graphs affect things like price, quantity, efficiency, and revenue. We hope you benefit from this resource, and happy studying!</p>
+        <h3> Carlos Pereira </h3>
+        <p>This website has been developed for the purpose of enriching economics curricula in high schools and universities. Students are encouraged to visualize the shifts associated with the economics concepts that they learn in their introduction and intermediate microeconomics and macroeconomics classes. The graphs that can be studied are displayed in the tab under the respective class. Please explore the different graphs and play around with the different factors to see how shifts in the graphs affect things like price, quantity, efficiency, and revenue. We hope you benefit from this resource, and happy studying!</p>
+
+        <h2> Endorsements </h2>
+        <p>This website has been developed for the purpose of enriching economics curricula in high schools and universities. Students are encouraged to visualize the shifts associated with the economics concepts that they learn in their introduction and intermediate microeconomics and macroeconomics classes. The graphs that can be studied are displayed in the tab under the respective class. Please explore the different graphs and play around with the different factors to see how shifts in the graphs affect things like price, quantity, efficiency, and revenue. We hope you benefit from this resource, and happy studying!</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+
+    </div>  <!-- .container-->
+  </div> <!-- .wrapper-->
+
+  <!-- <script src="flickity.min.js"></script>
+  <script>
+    window.onload = function() {
+      var elem = document.querySelector('.flickity');
+      var flkty = new Flickity(elem, {
+        contain: true,
+        wrapAround: true,
+        autoPlay: true,
+        prevNextButtons: false
+      });
+    }
+  </script> -->
+  <?php echo file_get_contents("html/footer.html"); ?>
